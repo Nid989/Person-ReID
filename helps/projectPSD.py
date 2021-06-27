0421238ff2a739671472904d5add2433612502d7
nid989 <nidbhavsar989@gmail.com>
2021-06-27 11:30:00 +0530

    deleted unnecessary content

--- helps/projectPSD.py
+++ helps/projectPSD.py
@@ -14,22 +14,3 @@ def projectPSD(M):
     d[d<=0] = np.finfo(np.float64).eps
     M = np.matmul(V, np.matmul(np.diag(d), V.T))
     return M
-    
-
-    # D, V = torch.eig(M, eigenvectors=True) # computes the eigenvectors and eigenvalues corresponding to M
-    # # remove complex part from both EigneValues and EigenVectors
-    # # Pytorch implementation of torch.eig(M) computes matrix D: n x 2 
-    # # where D[:,0] = real values and D[:,1] = complex values
-    # # V: n x n only computes real valued eigenvectors
-    # d = D[:, 0]
-    # for idx, i in enumerate(d):
-    #     if i<=0:
-    #         d[idx] = torch.finfo(torch.float64).eps
-    
-    # D = torch.diag(d).type(torch.double)
-    # V = V.type(torch.double)
-
-    # # Eignevalue decomposition of any matrix 'A' is given by
-    # # A = Vdiag(D)V'
-    # M = torch.mm(V, torch.mm(D, V.T))
-    # return M
